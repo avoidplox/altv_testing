2224fdc079d9865bdc2da2f7771cf9906ed9c6aa
Ryan Alvarez <avoidmeplox@gmail.com>
2021-10-19 16:32:33 +0200

    Vehicle spawning: * vehicle spawn with 'n' key * last vehicle deleted if more than 3

--- resources/vehicle_spawner/client/index.js
+++ resources/vehicle_spawner/client/index.js
@@ -1,11 +1,29 @@
 "use strict";
 /// <reference path="typings/altv-client.d.ts"/>
 /// <reference path="typings/natives.d.ts"/>
+/// <reference types="@altv/types-server" />
+
 import * as alt from 'alt';
 import * as native from 'natives';
 import * as chat from 'chat';
 
+const keys = {
+  't': 0x54,
+  'n': 0x4E
+};
+
 alt.on("keyup", (key) => {
-  chat.pushLine("Test");
+  if(key == keys['n'] && alt.gameControlsEnabled()){
+    alt.emitServer('vehicleSpawn');
+  };
 });
-//key === 0x54 && alt.gameControlsEnabled()
+
+alt.onServer('warpveh', (veh) => {
+  chat.pushLine('spawned')
+  alt.setTimeout(() => {
+      native.setPedIntoVehicle(native.playerPedId(), veh.scriptID, -1);
+  }, 500);
+});
+
+
+
--- resources/vehicle_spawner/server/index.js
+++ resources/vehicle_spawner/server/index.js
@@ -1,4 +1,27 @@
 import * as alt from "alt-server";
 
+const vehicles = ['issi3', 'felon', 'oracle2', 'fbi'];
+let spawnedVehicles = [];
+
+alt.onClient('vehicleSpawn', player => {
+    let randomVeh = vehicles[Math.floor(Math.random() * vehicles.length)];
+    let vehicle = spawnVehicle(player, randomVeh); // spawn random vehicle from array
+    vehicle.primaryColor = 50;
+    alt.emitClient(player, 'warpveh', vehicle);
+});
+
+function spawnVehicle(player, vehicleModel){
+    let veh;
+    veh = new alt.Vehicle(vehicleModel, player.pos.x, player.pos.y, player.pos.z, 0, 0, 0);
+    console.log(`${player.name} spawned a ${vehicleModel}.`);
+    spawnedVehicles.unshift(veh); // put vehicle into array at index 0
+    if (spawnedVehicles.length >= 4){ // only 3 vehicles per player
+        let vehicle;
+        vehicle = spawnedVehicles[spawnedVehicles.length - 1];
+        spawnedVehicles.pop(spawnedVehicles);
+        vehicle.destroy();
+    }
+    return veh;
+}
 
 
